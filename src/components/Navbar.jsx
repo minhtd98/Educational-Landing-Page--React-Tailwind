51bc6c5accd626768702e2993e06e190ff249e66
DuyMinh <minhtd98@gmail.com>
2023-09-26 11:47:22 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-scroll";
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="flex flex-row justify-between p-5 md:px-32 bg-white shadow-[0_3px_10px_rgb(0,0,0,0,0.2)]">
+        <div>
+          <Link to="/" className="font-semibold text-2xl p-1 cursor-pointer">
+            eStudy
+          </Link>
+        </div>
+        <nav className="hidden md:flex gap-5 font-medium p-1 cursor-pointer">
+          <Link
+            to="home"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-brightGreen transition-all cursor-pointer"
+          >
+            Home
+          </Link>
+          <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-brightGreen transition-all cursor-pointer"
+          >
+            About
+          </Link>
+          <Link
+            to="courses"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-brightGreen transition-all cursor-pointer"
+          >
+            Courses
+          </Link>
+          <Link
+            to="reviews"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-brightGreen transition-all cursor-pointer"
+          >
+            Reviews
+          </Link>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-brightGreen transition-all cursor-pointer"
+          >
+            Contact
+          </Link>
+        </nav>
+
+        <div className="flex md:hidden">
+          <div className="p-2">
+            <AiOutlineMenu size={22} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
